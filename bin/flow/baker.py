d0c37d7adbba2c57b7fef0f0fe4f172afc61917d
Nicolas RAMY <nicolas.ramy@gmail.com>
2013-12-06 09:03:12 +0100

    config parser

--- bin/flow/baker.py
+++ bin/flow/baker.py
@@ -57,6 +57,8 @@ def bake(configuration, recipes, env, branch="develop"):
 
 def install(configuration, recipes, env, branch):
     apps = ["cakephp", "angularjs", "flask"]
+    branch_slug = utils.slugify(branch)
+    domain_slug = configuration["project"]["name"] + "." + branch_slug + "." + configuration[env]["host"]
 
     if configuration[env]["app"]["type"] in apps:
         if configuration[env]["app"]["type"] == "angularjs":
@@ -66,15 +68,40 @@ def install(configuration, recipes, env, branch):
 
             web = {
                 "domain": utils.slugify(configuration["project"]["name"]) + "." +
-                utils.slugify(branch, "--") + "." + configuration[env]["host"]
+                utils.slugify(branch, "--") + "." + configuration[env]["host"],
+                "root_path": configuration[env]["workspace"] + "/" + configuration["project"]["name"]
             }
+            pprint.pprint(web)
 
+            # Create nginx_fpm configuration
+            if configuration[env]["app"]["web"] == "nginx_cakephp":
+                with open(recipes + "/web/nginx_cakephp/domain.conf") as fileout_resource:
+                    fileout = pystache.render(fileout_resource.read(), web)
+
+                    # Write nginx configuration
+                    workspace_path = "/etc/nginx/sites-available"
+                    filename = workspace_path + "/" + domain_slug
+                    add_file(configuration, env, branch, workspace_path, filename, fileout)
+
+                    # Symbolic link
+                    filename = domain_slug
+                    workspace_path_link = "/etc/nginx/sites-enabled"
+                    link_domain(configuration, env, workspace_path_link, filename)
+
+                    # Symbolic link
+                    service_nginx_reload(configuration, env)
+
+            # Create cakephp database
             with open(recipes + "/app/cakephp/database.php") as fileout_resource:
                 fileout = pystache.render(fileout_resource.read(), web)
-                pprint.pprint(fileout_resource.read())
-                pprint.pprint(web)
 
-            add_file(configuration, env, branch, fileout)
+                workspace_path = configuration[env]["workspace"] + "/" + configuration["project"]["name"] + \
+                    "/" + utils.slugify(branch) + "/app/Config"
+                filename = workspace_path + "/database.php"
+                if add_file(configuration, env, branch, workspace_path, filename, fileout):
+                    print "ok"
+                else:
+                    print "fail"
 
             # Mustache recipes
             print "pystache"
@@ -89,7 +116,7 @@ def install(configuration, recipes, env, branch):
     return False
 
 
-def add_file(configuration, env, branch, fileout):
+def add_file(configuration, env, branch, workspace_path, filename, fileout):
     """
     Add file
     configuration
@@ -104,9 +131,39 @@ def add_file(configuration, env, branch, fileout):
     fabric_initer(configuration, env)
 
     # Params
-    branch_slug = utils.slugify(branch)
-    workspace_path = utils.get_workspace_path(configuration, env, branch)
 
-    pprint.pprint(fileout)
+    # filename exists?
+    if api.env.role == "local":
+        return False
 
-    return False
+    else:
+        if not workspace_path:
+            return False
+
+        else:
+            # Write file
+            print "write file"
+            ffiles.append(filename, fileout)
+
+
+def link_domain(configuration, env, workspace_path_link, filename):
+    fabric_initer(configuration, env)
+
+    if api.env.role == "local":
+        return False
+
+    else:
+        with api.cd(workspace_path_link):
+            if not ffiles.exists(filename):
+                api.run("ln -s ../sites-available/%s" % filename)
+
+
+def service_nginx_reload(configuration, env):
+    fabric_initer(configuration, env)
+
+    if api.env.role == "local":
+        return False
+
+    else:
+        #api.run("/etc/init.d/nginx reload")
+        return True
